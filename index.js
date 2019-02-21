e87e79df27a41d41f56a6d2f9aad5cec2f35f9b5
Marcus Coleman <Sunhat@users.noreply.github.com>
2019-02-21 15:22:25 +0000

    Convert floats to int

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var { NativeModules, Platform } = require('react-native');
 if (Platform.OS === 'android') {
   module.exports = {
     get(dim) {
-      return NativeModules.ExtraDimensions[dim];
+      const result = NativeModules.ExtraDimensions[dim];
+      // If rounding down is ODD, round up. If rounding down is EVEN, round down.
+      return Math.floor(result) % 2 ? Math.ceil(result) : Math.floor(result);
     }
   };
 } else {
