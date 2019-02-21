7a0895087da305179f5d69a613415e72fa2db1d7
Marcus Coleman <Sunhat@users.noreply.github.com>
2019-02-21 17:26:36 +0000

    IMPORTANT FIX

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ if (Platform.OS === 'android') {
   module.exports = {
     get(dim) {
       try {
-        if(!NativeModules.hasOwnProperty('ExtraDimensions')) {
+        if(!NativeModules.ExtraDimensions) {
           throw "ExtraDimensions not defined. Try rebuilding your project. e.g. react-native run-android"
         }
         const result = NativeModules.ExtraDimensions[dim];
+
+        if(typeof result !== 'number') {
+            return result;
+        }
+        // If rounding down is ODD, round up. If rounding down is EVEN, round down.
+        return Math.floor(result) % 2 ? Math.ceil(result) : Math.floor(result);
       } catch (e) {
         console.error(e)
       }
-      if(typeof result !== 'number') {
-          return result;
-      }
-      // If rounding down is ODD, round up. If rounding down is EVEN, round down.
-      return Math.floor(result) % 2 ? Math.ceil(result) : Math.floor(result);
     }
   };
 } else {
